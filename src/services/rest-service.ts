be2d7ddd7308962bb1637be5a759567077b03eb0
Tielman <tielman.mailbox@gmail.com>
2018-04-04 09:39:32 -0400

    dep change

--- src/services/rest-service.ts
+++ src/services/rest-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/Observable/of';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class RestService {
